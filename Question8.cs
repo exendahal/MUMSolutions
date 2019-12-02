cc67208784fa1d05ab4106e4103311d47b6ccc4b
Santosh Dahal <exendahal@yahoo.com>
2019-12-02 16:54:38 +0545

    Add files via upload

---
+++ Question8.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 13, 6, 3, 2 } ;
+            int ret = isSub(a);
+            int isSub(int[] a)
+            {
+
+                for (int i=0;i<a.Length;i++)
+                {
+                    int currentValue = a[i];
+
+                    //Paxadi ko sum nikalne
+                    int sum = 0;
+                    for (int j=i+1;j<a.Length;j++)
+                    {
+                        sum = sum + a[j] ;
+                    }
+
+                    if (currentValue < sum)
+                    {
+                        return 0;
+                    }
+                                
+                }
+                return 1;
+
+            }
+        }
+    }
+}
