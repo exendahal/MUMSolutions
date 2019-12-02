fd9be28bb42e3f53fbdfff47d93b614028b1658b
Santosh Dahal <exendahal@yahoo.com>
2019-12-02 09:28:37 +0545

    Add files via upload
    Question 1 solution

---
+++ Question1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int m = 32;
+            int n = 18;
+            int aaa= check(m , n);
+            int check(int a,int b) {
+                for (int i = 2; i < m; i++)
+                {
+                    if (i % 2 == 0 && m % i == 0 && n % i != 0)
+                    {
+                        return 0;
+                    }                   
+                   
+                }
+                return 1;
+            }
+           
+        }
+    }
+}
