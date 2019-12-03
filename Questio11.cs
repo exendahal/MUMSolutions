79ea20b3e313cd8801aba083cc69d0b4cafc6ab5
Santosh Dahal <exendahal@yahoo.com>
2019-12-03 08:43:22 +0545

    Add files via upload

---
+++ Questio11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 8,3,4};
+            int ret = isMeera(array);
+
+            int isMeera(int[] a)
+            {
+                for (int i=0;i<a.Length;i++)
+                {
+                    for (int j=0;j<a.Length;j++)
+                    {
+                        if (a[i]==(2*a[j]))
+                        {
+                            return 0;
+                        }
+                    }
+                }
+                return 1;
+            }
+        }
+         
+    }
+}
