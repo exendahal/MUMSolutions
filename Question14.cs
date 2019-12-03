25e60a7ea7066ed92fda4cba1adc4a3bd005bda9
Santosh Dahal <exendahal@yahoo.com>
2019-12-03 11:00:04 +0545

    Add files via upload

---
+++ Question14.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int aa = 21;
+            int ret = isMeera(aa);
+            int isMeera(int n)
+            {
+                int numberOfFactors = 0;
+                for (int i=2;i<=n/2;i++)
+                {
+                    if (n%i==0)
+                    {
+                        numberOfFactors++;
+                    }
+                }
+                if (n%numberOfFactors!=0)
+                {
+                    return 0;
+                }
+                return 1;
+
+            }
+
+
+        }
+         
+    }
+}
