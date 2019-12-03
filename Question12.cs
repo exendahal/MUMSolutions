5c8c3a57c1aac90e6e3fab676a88d254bf648308
Santosh Dahal <exendahal@yahoo.com>
2019-12-03 09:00:26 +0545

    Add files via upload

---
+++ Question12.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int aa = 18;
+            int ret = factorTwoCount(aa);
+
+            int factorTwoCount(int n)
+            {
+                int numberofDiv = 0;
+               
+                while (n>1)
+                {
+                    if (n%2==0)
+                    {
+                        numberofDiv++;
+                        n = n / 2;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                return numberofDiv;
+            }
+        }
+         
+    }
+}
