45020744ffa100c7fcb970747070f97cba76b3bd
Santosh Dahal <exendahal@yahoo.com>
2019-12-03 15:19:47 +0545

    Add files via upload

---
+++ Question16.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int aa = 1001;
+            int[] arr = {4, 11, 12, 1, 6};
+            int ret = isWave(arr);
+            int isWave(int[] a)
+            {
+                for (int i=0;i<(a.Length-1);i++)
+                {
+                    if (a[i]%2==0 && a[i+1]%2==0)
+                    {
+                        return 0;
+                    }
+                    else if(a[i] % 2 != 0 && a[i + 1] % 2 != 0)
+                    {
+                        return 0;
+                    }
+                }
+
+                return 1;
+            }
+        }
+         
+    }
+}
