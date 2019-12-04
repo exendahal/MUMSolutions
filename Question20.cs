0b7d8e761a9d8a1524783f2bdce2cef4ef2dc349
Santosh Dahal <exendahal@yahoo.com>
2019-12-04 19:11:28 +0545

    Add files via upload

---
+++ Question20.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 22;
+
+            int ret = isFibonacci(num);
+
+            int isFibonacci(int n)
+            {
+                int tempVal = (5 * n * n) + 4;
+                int tempVal2 = (5 * n * n) - 4;
+                double val = Math.Sqrt(tempVal);
+                double val2 = Math.Sqrt(tempVal2);
+                if (val%1==0 || val2 % 1 == 0)
+                {
+                    return 1;
+                }
+                return 0;
+            }
+
+
+        }
+
+    }
+}
