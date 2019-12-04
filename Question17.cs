95df163469a8aeb91ebc7fbe26161301b9604872
Santosh Dahal <exendahal@yahoo.com>
2019-12-04 09:12:33 +0545

    Add files via upload

---
+++ Question17.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int aa = -3333531;
+            int ret = countDigit(aa,3);
+            int countDigit(int n, int digit)
+            {
+                int count = 0;
+                int number = n;
+                if (n<0)
+                {
+                    n = -n;
+                }
+                while (n>1)
+                {
+                    if (n % 10 == digit)
+                    {
+                        count++;
+                    }
+                    n=n / 10;
+                }
+                if (number<0)
+                {
+                    return -count;
+                }
+                else
+                {
+                    return count;
+                }
+               
+            }
+        }
+         
+    }
+}
