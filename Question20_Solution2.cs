956efa9d9e4baca3b450d1c978d31d9a1d702c37
Santosh Dahal <exendahal@yahoo.com>
2019-12-05 21:12:07 +0545

    Add files via upload

---
+++ Question20_Solution2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ret = isFancy(55);
+            int isFancy(int n)
+            {
+                if (n == 1)
+                    return 1;
+
+                int fancyNumber = 0;
+                int pprevious = 1;
+                int previous = 1;
+
+                while (fancyNumber < n)
+                {
+                    fancyNumber = ((previous) + (pprevious));
+                    pprevious = previous;
+                    previous = fancyNumber;
+                }
+
+                if (fancyNumber == n)
+                    return 1;
+
+                return 0;
+            }
+        }
+
+    }
+}
