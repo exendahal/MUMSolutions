4b775fb93b68d2441f052a5ed96aabfd1fe556e7
Santosh Dahal <exendahal@yahoo.com>
2019-12-05 09:48:35 +0545

    Add files via upload

---
+++ Question22.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = -250;
+            int secnum = 5;
+            int ret = getExponent(num, secnum);
+
+            int getExponent(int n, int p)
+            {
+                int count = 0;
+                if (n<0)
+                {
+                    n = -n;
+                }
+                if (p <= 1)
+                {
+                    return -1;
+                }
+
+                while (n>1)
+                {
+                    if (n%p==0)
+                    {
+                        count++;
+                        n = n / p;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                return count;
+            }
+
+
+        }
+
+    }
+}
