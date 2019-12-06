a23b53931415450f5210f63cda618eeed26988ab
Santosh Dahal <exendahal@yahoo.com>
2019-12-06 11:17:05 +0545

    Add files via upload

---
+++ Question9_solution2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 9, 8, 7, 13, 14, 17 };
+
+              int vvv = goodSpread(arr);
+
+            int goodSpread(int[] a)
+            {
+                for (int i = 0, j = a.Length - 1; i < j; i++, j--)
+                {
+                    if ((a[i] % 2 == 0 && a[j] % 2 != 0) || (a[i] % 2 == 1 && a[j] % 2 != 1))
+                    {
+                        return 0;
+                    }
+                    
+                }
+                return 1;
+
+            }
+
+        }
+    }
+}
